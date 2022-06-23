3f08695b1995299dbcb7e0ecb6d5311e773fb0e9
kelemii <98933496+kelemii@users.noreply.github.com>
2022-06-23 14:57:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+var container = document.createElement('div');
+document.body.append(container);
+
+    for (var i=0;i<81;i++){
+        if (i % 2 === 0) {
+                var div = document.createElement('div');
+                div.style.width = '11.1%';
+                div.style.float = "left";
+                div.style.paddingBottom = '11.1%';  
+                div.style.background = "black";
+                container.append(div);
+        } else {
+            var div = document.createElement('div');
+            div.style.width = '11.1%';
+            div.style.float = "left";
+            div.style.paddingBottom = '11.1%';  
+            div.style.background = "red";
+            container.append(div);
+}
+    }
